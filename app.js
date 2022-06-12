98f9fbb50d907d318828b90f8d24093641f3440f
Liridon Loku <liridonloku@gmail.com>
2022-06-12 17:00:19 +0200

    Add user model and set up passport local strategy

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const compression = require("compression");
 const helmet = require("helmet");
+const passport = require("passport");
+const LocalStrategy = require("passport-local").Strategy;
+const bcrypt = require("bcryptjs");
+const User = require("./models/user");
 
 const mongoDb = process.env.MONGODB_URI;
 mongoose.connect(mongoDb, { useUnifiedTopology: true, useNewUrlParser: true });
@@ -22,6 +26,44 @@ const app = express();
 // view engine setup - TODO: Remove
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "pug");
+
+passport.use(
+  new LocalStrategy(
+    {
+      usernameField: "email",
+      passwordField: "password",
+    },
+    (email, password, done) => {
+      User.findOne({ email: email }, (err, user) => {
+        if (err) {
+          return done(err);
+        }
+        if (!user) {
+          return done(null, false, { message: "Incorrect email" });
+        }
+        bcrypt.compare(password, user.password, (err, res) => {
+          if (res) {
+            // passwords match! log user in
+            return done(null, user);
+          } else {
+            // passwords do not match!
+            return done(null, false, { message: "Incorrect password" });
+          }
+        });
+      });
+    }
+  )
+);
+
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    done(err, user);
+  });
+});
 
 app.use(logger("dev"));
 app.use(express.json());
