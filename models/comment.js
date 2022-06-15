25390aad8ae0bd1d55a408777565524f23bfdae7
Liridon Loku <liridonloku@gmail.com>
2022-06-15 19:02:08 +0200

    Add comment model

---
+++ models/comment.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  post: { type: Schema.Types.ObjectId, ref: "Post", required: true },
+  user: { type: String, required: true },
+  content: { type: String, required: true },
+  date: { type: Date, required: true },
+});
+
+CommentSchema.virtual("url").get(function () {
+  return "/posts/" + this.post._id + "/comments/" + this._id;
+});
+
+module.exports = mongoose.model("Comment", CommentSchema);
