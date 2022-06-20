e1d9ca1299ee037a2c1c34d8ab991369201965c3
Liridon Loku <liridonloku@gmail.com>
2022-06-20 17:05:36 +0200

    Add headers for cors

--- app.js
+++ app.js
@@ -73,6 +73,15 @@ app.use(compression());
 app.use(helmet());
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 app.use("/", indexRouter);
 app.use("/api", apiRouter);
 app.use("/users", usersRouter);
