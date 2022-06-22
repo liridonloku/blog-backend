6fa3f9054332f0ffa07a33cc029df4c16f9d850d
Liridon Loku <liridonloku@gmail.com>
2022-06-22 17:23:02 +0200

    Allow all methods on access control

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
